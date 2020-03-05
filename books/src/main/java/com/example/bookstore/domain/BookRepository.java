cdb06057bd0b2889cb789093857827901f6db690
Toivonen Juho <bgh299@myy.haaga-helia.fi>
2020-03-05 14:32:53 +0200

    Chapter 5, tehtävä 2 muutokset: spring rest data

--- books/src/main/java/com/example/bookstore/domain/BookRepository.java
+++ books/src/main/java/com/example/bookstore/domain/BookRepository.java
@@ -2,8 +2,11 @@ package com.example.bookstore.domain;
 
 import java.util.List;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
 import com.example.bookstore.domain.Book;
 
+@RepositoryRestResource
 public interface BookRepository extends CrudRepository<Book, Long>{
 	
 	List<Book> findByTitle(String title);
